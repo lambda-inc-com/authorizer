1ea7fecfe0cd7cc74e1b491cf5584f1954890ebe
派大星 <ybf0711@gmail.com>
2025-07-08 21:07:35 +0800

    temp

--- server/handlers/authorize.go
+++ server/handlers/authorize.go
@@ -68,6 +68,7 @@ const (
 // code_challenge_method = to prevent CSRF attack [only sh256 is supported]
 func AuthorizeHandler() gin.HandlerFunc {
 	return func(gc *gin.Context) {
+		log.Println("++++++++++++++++66666")
 		log.Println("++++++++++++++++++++++++")
 		redirectURI := strings.TrimSpace(gc.Query("redirect_uri"))
 		responseType := strings.TrimSpace(gc.Query("response_type"))
