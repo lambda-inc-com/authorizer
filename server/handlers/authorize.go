eb261c09eaf3dc4c737d7ba091b40db9646f9c88
包包 <2271948415@qq.com>
2025-07-08 18:02:55 +0800

    修改

--- server/handlers/authorize.go
+++ server/handlers/authorize.go
@@ -68,6 +68,7 @@ const (
 // code_challenge_method = to prevent CSRF attack [only sh256 is supported]
 func AuthorizeHandler() gin.HandlerFunc {
 	return func(gc *gin.Context) {
+		log.Println("++++++++++++++++++++++++")
 		redirectURI := strings.TrimSpace(gc.Query("redirect_uri"))
 		responseType := strings.TrimSpace(gc.Query("response_type"))
 		state := strings.TrimSpace(gc.Query("state"))
