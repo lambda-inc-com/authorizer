3d78ca1bce4ffadc792a5a1df31c4f7046d89f39
包包 <2271948415@qq.com>
2025-07-11 13:54:13 +0800

    修改

--- server/routes/routes.go
+++ server/routes/routes.go
@@ -40,8 +40,8 @@ func InitRouter(log *logrus.Logger, aiGRPCClient *services.AIGRPCClient) *gin.En
 	// login page app related routes.
 	app := router.Group("/app")
 	{
-		app.Static("/favicon_io", "/app/favicon_io")
-		app.Static("/build", "/app/build")
+		app.Static("/favicon_io", "./app/favicon_io")
+		app.Static("/build", "./app/build")
 		app.GET("/", handlers.AppHandler())
 		app.GET("/:page", handlers.AppHandler())
 	}
@@ -49,9 +49,9 @@ func InitRouter(log *logrus.Logger, aiGRPCClient *services.AIGRPCClient) *gin.En
 	// dashboard related routes
 	dashboard := router.Group("/dashboard")
 	{
-		dashboard.Static("/favicon_io", "/dashboard/favicon_io")
-		dashboard.Static("/build", "/dashboard/build")
-		dashboard.Static("/public", "/dashboard/public")
+		dashboard.Static("/favicon_io", "./dashboard/favicon_io")
+		dashboard.Static("/build", "./dashboard/build")
+		dashboard.Static("/public", "./dashboard/public")
 		dashboard.GET("/", handlers.DashboardHandler())
 		dashboard.GET("/:page", handlers.DashboardHandler())
 	}
